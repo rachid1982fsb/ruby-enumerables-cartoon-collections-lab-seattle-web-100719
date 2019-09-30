03b43b7297d85c6d4c29fbf6d588e5e648e15b8e
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-09-30 21:52:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 
 def long_planeteer_calls(planeteer) # code an argument here
   # Your code here
-  planeteer.all{|call| 
+  planeteer.all{|call| call.lenght
 end
 
 def find_the_cheese# code an argument here
